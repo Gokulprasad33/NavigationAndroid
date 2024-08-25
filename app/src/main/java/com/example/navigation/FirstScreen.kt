c6d59ebf7caa6448c84d6596d81c9f1e021d396a
gokulprasad33 <gokulprasad2k5@gmail.com>
2024-08-25 14:37:03 +0530

    Navigation-Android

--- app/src/main/java/com/example/navigation/FirstScreen.kt
+++ app/src/main/java/com/example/navigation/FirstScreen.kt
@@ -16,6 +16,7 @@ import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 
+
 @Composable
 fun FirstScreen(navigateToSecondScreen:()->Unit){
     val name= remember {
